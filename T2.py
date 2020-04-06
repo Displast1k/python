64feab420eed9ecacba31608eb6daeec425a1a03
Displast1k <displast1k@mail.ru>
2020-04-06 17:07:43 +0300

    release v1

---
+++ T2.py
@@ -0,0 +1,22 @@
+#Калькулятор v2
+from colorama import init	
+from colorama import Fore,Back,Style
+
+init()
+print( Back.GREEN )
+what = input ( "Что делаем? (+,-) : " )
+
+a = float ( input("Введи первое число: "))
+b = float ( input("Введи второе число: "))
+
+if what == "+":
+	c = a + b
+	print ("Результат: " + str(c))
+
+elif what == "-":
+	c = a - b
+	print ("Результат: " + str(c))
+
+else:
+	print("Выбрана неверная операция")
+
